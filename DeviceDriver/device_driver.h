b7f3238624041affbf981368645ef8463d5cdda8
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-24 15:02:50 +0900

    base code

---
+++ DeviceDriver/device_driver.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "flash_memory_device.h"
+
+class DeviceDriver
+{
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+protected:
+    FlashMemoryDevice* m_hardware;
+};
